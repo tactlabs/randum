3fb27c9fa12eae204a6737518febe564e236b6a4
Talha-Altair <cooltalha27@gmail.com>
2021-10-15 19:16:47 +0530

    basic clothing code added

--- dummy.py
+++ dummy.py
@@ -1,12 +1,10 @@
-
 import randum
 
 def startpy():
 
     rand_ = randum.Randum()
     
-    print(rand_.name())
-
+    print(rand_.men_clothing())
 
 if __name__ == '__main__':
     startpy()
--- randum/providers/clothing/__init__.py
+++ randum/providers/clothing/__init__.py
@@ -8,44 +8,20 @@ class Provider(BaseProvider):
     To generate Indian CLothing
     """
 
-    def simple_profile(self, sex=None):
-        """
-        Generates a basic profile with personal informations
-        """
-        SEX = ["F", "M"]
-        if sex not in SEX:
-            sex = self.random_element(SEX)
-        if sex == 'F':
-            name = self.generator.name_female()
-        elif sex == 'M':
-            name = self.generator.name_male()
-        return {
-            "username": self.generator.user_name(),
-            "name": name,
-            "sex": sex,
-            "address": self.generator.address(),
-            "mail": self.generator.free_email(),
-            "birthdate": self.generator.date_of_birth(),
-        }
+    def clothing(self):
 
-    def clothing(self, fields=None, sex=None):
-      
-        if fields is None:
-            fields = []
+        clothes = ['suit','tie','shoes','belt','hat']
 
-        d = {
-            "job": self.generator.job(),
-            "company": self.generator.company(),
-            "ssn": self.generator.ssn(),
-            "residence": self.generator.address(),
-            "current_location": (self.generator.latitude(), self.generator.longitude()),
-            "blood_group": "".join(self.random_element(list(itertools.product(["A", "B", "AB", "O"], ["+", "-"])))),
-            "website": [self.generator.url() for _ in range(1, self.random_int(2, 5))],
-        }
+        result = self.random_element(clothes)
 
-        d = dict(d, **self.generator.simple_profile(sex))
-        # field selection
-        if len(fields) > 0:
-            d = {k: v for k, v in d.items() if k in fields}
+        return result
 
-        return d
+    def men_clothing(self):
+
+        men_clothes = ['a','b']
+
+        result = self.random_element(men_clothes)
+
+        return result
+
+
