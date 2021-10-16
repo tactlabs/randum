130424d90d4c8d0b2f0c3305ebf77c6cfbae7e35
Sanjjushri <sanjjushrivarshini@gmail.com>
2021-10-16 18:00:54 +0530

    technology

--- dummy.py
+++ dummy.py
@@ -4,11 +4,11 @@ def startpy():
 
     rand_ = randum.Randum()
     
-    # print(rand_.men_clothing())
     # print(rand_.indian_food())
     # print(rand_.italian_food())
     print(rand_.middle_name())
     print(rand_.girl_middle_names())
+    print(rand_.technology())
 
 if __name__ == '__main__':
     startpy()
---
+++ randum/providers/technology/__init__.py
@@ -0,0 +1,24 @@
+import itertools
+
+from .. import BaseProvider
+
+
+class Provider(BaseProvider):
+    """
+    To generate Technology
+    """
+
+    def technology(self):
+
+        tech = ['5G Technology','Internet of Behaviours(IoB)','DevSecOps','Intelligent Process Automation(IPA)','Tactile VR',
+                'Big Data Analytics','Human Augmentation','Everything-as-a-Service(XaaS)','Cybersecurity','Data Science',
+                'Machine Learning','Robotic Process Automation (RPA)','Edge Computing','Quantum Computing','Virtual Reality and Augmented Reality',
+                'Blockchain','Internet of Things (IoT)','Wearables and augmented humans','Big Data and augmented analytics','Intelligent spaces and smart places',
+                'Cloud and edge computing','Digitally extended realities',' Digital twins','Natural language processing','Voice interfaces and chatbots',
+                'Computer vision and facial recognition','Robots and cobots','Autonomous vehicles',' Genomics and gene editing','Machine co-creativity and augmented design',
+                'Digital platforms', 'Deep Learning'
+               ]
+
+        result = self.random_element(tech)
+
+        return result
